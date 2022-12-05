a51e1ef3eb4ce68f3178affabf4bb3483d02518d
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2022-12-05 16:31:42 +0100

    set up project

---
+++ app/Http/Controllers/UserAuthController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserAuthController extends Controller
+{
+    public function SignUp(Request $request)
+    {
+    }
+
+    public function SignIn(Request $request)
+    {
+    }
+
+    public function SignUpWithFacebook(Request $request)
+    {
+    }
+
+    public function SignInWithFacebook(Request $request)
+    {
+    }
+
+    public function SignUpWithGoggle(Request $request)
+    {
+    }
+
+    public function SignInWithGoogle(Request $request)
+    {
+    }
+}
