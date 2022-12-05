b94f7d63e746811607f95bd0f57d665c26bbbb8c
FathiAyari <fathi.ayari@kelibia.r-iset.tn>
2022-12-05 16:43:56 +0100

    set up auth routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserAuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::post('/sign-up', [UserAuthController::class, 'SignUp']);
+Route::post('/sign-in', [UserAuthController::class, 'SignIn']);
+Route::post('/sign-up-with-facebook', [UserAuthController::class, 'SignUpWithFacebook']);
+Route::post('/sign-in-with-facebook', [UserAuthController::class, 'SignInWithFacebook']);
+Route::post('/sign-up-with-google', [UserAuthController::class, 'SignUpWithGoggle']);
+Route::post('/sign-in-with-google', [UserAuthController::class, 'SignInWithGoogle']);
+
